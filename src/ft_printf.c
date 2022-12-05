ef520cfc685a8c31cfc941f8b6b74d5e31d7333a
nokosse <visouthivong.kevin@gmail.com>
2022-12-05 12:01:45 +0100

    init

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,22 +6,14 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 14:59:42 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/12/05 11:08:30 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/12/05 11:59:22 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printf(char *str, ...)
+int ft_printf(const char *format, ...)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '%')
-		{
-			checkarg = 
-		}
-	}
+	va_list		args; //Declare la structure 'args' de type va_list.
+	va_start	(args, format); //Initialise la structure va_list (args)
 }
