f3b6df16252feb2575580477ed25ad77e7a081b5
nokosse <visouthivong.kevin@gmail.com>
2022-12-05 16:22:47 +0100

    edit printf

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 14:59:42 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/12/05 13:22:19 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/12/05 16:22:01 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void ft_parse(const char letter)
+static void	ft_parse(const char letter)
 {
 	if (letter == 'c')
 		ft_percent_c();
@@ -21,9 +21,9 @@ static void ft_parse(const char letter)
 	else if (letter == 'p')
 		ft_percent_p();
 	else if (letter == 'd')
-		ft_percent_d();
+		ft_percent_id();
 	else if (letter == 'i')
-		ft_percent_i();
+		ft_percent_id();
 	else if (letter == 'u')
 		ft_percent_u();
 	else if (letter == 'x')
@@ -31,31 +31,35 @@ static void ft_parse(const char letter)
 	else if (letter == 'X')
 		ft_percent_X();
 	else if (letter == '%')
-		//putchar %
+		ft_putchar_fd(letter, 1);
 }
 
-int	check_legal(const char letter) //dis si on retourne une lettre de l'exo
+int	check_legal(const char letter)
 {
-	const char legal = "cspdiuxX%";
-	//strchr doit retourner 1 si c bon
+	if (letter == 'c' || letter == 's' || letter == 'p' || letter == 'd'
+		|| letter == 'i' || letter == 'u' || letter == 'x' || letter == 'X'
+		|| letter == '%')
+		return (1);
+	return (0);
 }
 
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-	int	i;						//Le compteur que printf va retourner
-	va_list		args; 			//Declare la structure 'args' de type va_list.
-	va_start	(args, format); //Initialise la structure va_list (args)
+	int			i;
+	va_list		args;
 
+	va_start (args, format);
 	i = 0;
 	while (format[i])
 	{
 		if (format[i] == '%')
-			{
-				if (check_legal(format[i + 1]) == 0)
-					return (NULL);
-				else ()
-			}
+		{
+			if (check_legal(format[i + 1]) == 0)
+				return (NULL);
+			else
+			
+		}
 	}
 }
 
-printf("bonjour je suis %s jai %d ans jhabite dans le %s", prenom, age, ville)
+//printf("bonjour je suis %s jai %d ans jhabite dans le %s", prenom, age, ville)
