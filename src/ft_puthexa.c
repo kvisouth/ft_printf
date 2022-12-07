ff4ac8161c5f604f7d22a0fed8998a21f912eb74
nokosse <visouthivong.kevin@gmail.com>
2022-12-07 11:57:31 +0100

    return len of hex

--- src/ft_puthexa.c
+++ src/ft_puthexa.c
@@ -6,21 +6,21 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 19:39:17 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/12/07 11:53:38 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/12/07 11:57:19 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/libftprintf.h"
 
-int	ft_putnbr_hex(unsigned long nbr, char bs)
+int	ft_puthex(unsigned long nbr, char bs)
 {
 	int	len;
 
 	len = 0;
 	if (nbr >= 16)
 	{
-		len += ft_putnbr_hex((nbr / 16), bs);
-		len += ft_putnbr_hex((nbr % 16), bs);
+		len += ft_puthex((nbr / 16), bs);
+		len += ft_puthex((nbr % 16), bs);
 	}
 	else
 	{
@@ -36,6 +36,6 @@ int	ft_putnbr_hex(unsigned long nbr, char bs)
 // {
 // 	if (ac == 2)
 // 	{
-// 		printf("\nlen : %d\n",ft_putnbr_hex(atoi(av[1]),'X'));
+// 		printf("\nlen : %d\n",ft_puthex(atoi(av[1]),'X'));
 // 	}
 // }
