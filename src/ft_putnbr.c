95e00391079a6b5405e16b88b8bfb55c964e9a46
nokosse <visouthivong.kevin@gmail.com>
2022-12-07 11:36:57 +0100

    returning len of nb

--- src/ft_putnbr.c
+++ src/ft_putnbr.c
@@ -6,47 +6,30 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 22:43:11 by nok               #+#    #+#             */
-/*   Updated: 2022/12/06 17:08:39 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/12/07 11:36:37 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/libftprintf.h"
 
-static int	ft_putnbr_write(long int nb)
+int	ft_putnbr(long int nb)
 {
-	int	a;
+	int	len;
 
-	a = 1;
+	len = 0;
 	if (nb < 0)
 	{
-		a = -1;
-		ft_putchar('-');
+		len += ft_putchar('-');
 		nb *= -1;
 	}
 	if (nb >= 10)
 	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
+		len += ft_putnbr(nb / 10);
+		len += ft_putnbr(nb % 10);
 	}
 	else
-		ft_putchar(nb + 48);
-	return (nb * a);
-}
-
-int	ft_putnbr(long int nb)
-{
-	int	count;
-	
-	nb = ft_putnbr_write(nb);
-	count = 0;
-	if (nb < 0)
-		count++;
-	while (nb != 0)
-	{
-		nb = nb / 10;
-		count++;
-	}
-	return (count);
+		len += ft_putchar(nb + 48);
+	return (len);
 }
 
 // int main (int ac, char **av)
